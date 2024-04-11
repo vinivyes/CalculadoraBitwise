ebccf1d2352d294dc5169d0b17b5a383ebf88aae
Vinicius de Morais <viniciusmorais352@gmail.com>
2024-04-11 09:51:23 -0600

    Add project files.

---
+++ CasoEstudio2/Models/Calculadora.cs
@@ -0,0 +1,10 @@
+﻿namespace CasoEstudio2.Models
+{
+    public class Calculadora
+    {
+        public string Operacion { get; set; } = "AND";
+        public string BaseNumero1 { get; set; } = "Binario";
+        public string BaseNumero2 { get; set; } = "Binario";
+        public string BaseResultado { get; set; } = "Binario";
+    }
+}
